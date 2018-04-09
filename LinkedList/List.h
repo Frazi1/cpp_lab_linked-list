15c0334d6b870eb6e1709f076ce2f0aaa64acc76
Fraz <diman896@gmail.com>
2018-04-09 20:22:05 +0300

    init commit

---
+++ LinkedList/List.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <functional>
+
+using namespace std;
+
+template <class T>
+class list
+{
+private:
+	node<T>* first_;
+
+	itera
+	node<T>* find_last();
+public:
+	void add(T* item);
+	void remove(function<bool(T*)> predicate);
+};
+
+template <class T>
+node<T>* list<T>::find_last()
+{
+	node<T>* curr = first_;
+	if (curr == nullptr)
+		return nullptr;
+	while (curr->get_next() != nullptr)
+		curr = curr->get_next();
+	return curr;
+}
+
+template <class T>
+void list<T>::add(T* item)
+{
+	node<T>* new_node = new node<T>(item);
+	if (first_ == nullptr)
+	{
+		first_ = new_node;
+	}
+	else
+	{
+		node<T>* last = find_last();
+		last->set_next(new_node);
+		new_node->set_prev(last);
+	}
+}
---
+++ LinkedList/Node.h
@@ -0,0 +1,60 @@
+﻿#pragma once
+
+template <class T>
+class node
+{
+private:
+	node<T>* next_;
+	node<T>* prev_;
+	T* value_;
+
+public:
+
+	explicit node(T* value)
+		: value_(value)
+	{
+	}
+
+	node<T>* get_next() const;
+	void set_next(node<T>* const next);
+	node<T>* get_prev() const;
+	void set_prev(node<T>* const prev);
+	T* get_value() const;
+	void set_value(T* const value);
+};
+
+template <class T>
+node<T>* node<T>::get_next() const
+{
+	return next_;
+}
+
+template <class T>
+void node<T>::set_next(node<T>* const next)
+{
+	next_ = next;
+}
+
+template <class T>
+node<T>* node<T>::get_prev() const
+{
+	return prev_;
+}
+
+template <class T>
+void node<T>::set_prev(node<T>* const prev)
+{
+	prev_ = prev;
+}
+
+template <class T>
+T* node<T>::get_value() const
+{
+	return value_;
+}
+
+template <class T>
+void node<T>::set_value(T* const value)
+{
+	value_ = value;
+}
