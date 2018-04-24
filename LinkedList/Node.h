de1e68616786ee42f106f9601d8a1a554276d271
Fraz <diman896@gmail.com>
2018-04-24 08:41:35 +0300

    Node.h refactoring

--- LinkedList/Node.h
+++ LinkedList/Node.h
@@ -9,62 +9,41 @@ private:
 	T value_;
 
 public:
-
-	explicit node(T value);
+	explicit node(T value) : value_(value) {}
+	
 	~node()
 	{
 		if (next_ != nullptr) delete next_;
 	}
 
-	node<T>* get_next() const;
-	node<T>* get_prev() const;
-	T& get_value();
-	void set_next(node<T>* next);
-	void set_prev(node<T>* prev);
-	void set_value(T* value);
+	node<T>* get_next() const
+	{
+		return next_;
+	}
+
+	node<T>* get_prev() const
+	{
+		return prev_;
+	}
+
+	T& get_value()
+	{
+		return value_;
+	}
+
+	void set_next(node<T>* next)
+	{
+		next_ = next;
+	}
+	void set_prev(node<T>* prev)
+	{
+		prev_ = prev;
+	}
+	void set_value(T* value)
+	{
+		value_ = value;
+	}
 };
-
-template <class T>
-node<T>::node(T value) : value_(value)
-{
-}
-
-template <class T>
-node<T>* node<T>::get_next() const
-{
-	return next_;
-}
-
-template <class T>
-node<T>* node<T>::get_prev() const
-{
-	return prev_;
-}
-
-template <class T>
-T& node<T>::get_value()
-{
-	return value_;
-}
-
-template <class T>
-void node<T>::set_next(node<T>* next)
-{
-	next_ = next;
-}
-
-template <class T>
-void node<T>::set_prev(node<T>* prev)
-{
-	prev_ = prev;
-}
-
-template <class T>
-void node<T>::set_value(T* value)
-{
-	value_ = value;
-}
-
 
 
 
